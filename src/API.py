b2aeb4c0014dd74e1f205304a69fe228021679ed
wcqaguxa <a.k.wolska@student.tudelft.nl>
2022-04-30 21:22:08 +0200

    nicify a bit

--- src/API.py
+++ src/API.py
@@ -5,15 +5,6 @@ import numpy as np
 from sklearn.neighbors import KDTree
 
 
-# input: null or area
-# każdy kraj zawiera POI. POI to albo cel albo linia.
-# osobno jest lista krajów
-
-# jak zmapować kraje do współrzędnych?
-# the list of all countries. might be read from a dataset?
-# countries = {"Netherlands": {"W":, "E":, "S"}}
-#
-
 
 class API:
     station_locations: KDTree
@@ -33,8 +24,8 @@ class API:
         self.station_locations = KDTree(np.array(self.data))
 
     def get_closest_station(self, point: dict) -> Station:
-        new_point = Point(point["lat"], point["lgn"])
-        ind = self.station_locations.query([[point["lat"], point["lgn"]]], return_distance=False)
+        new_point = [[point["lat"], point["lgn"]]]
+        ind = self.station_locations.query(new_point, return_distance=False)
         point = self.data[ind[0][0]]
         point = Point(point[0], point[1])
         result = self.stations.get(point)

501c6eb1f93e97684adf982a93d54a5dd78d4a24
Yellowisharo <76267014+Yellowisharo@users.noreply.github.com>
2022-04-30 17:59:48 +0200

    some fixes, trial run

--- src/lptravelplanner.py
+++ src/lptravelplanner.py
@@ -1,28 +1,49 @@
-import pulp as *
+from pulp import *
 
 
-def lpsolver(input_data):
-    model = LpProblem("Minimize costs or length", LpMinimize)
+def lpsolver(stations,tracks,choicepoints,choicetracks,length,costs,A,B):
+    model = LpProblem("Minimize_costs_or_length", LpMinimize)
+    nrstations = len(stations)
 
     P = LpVariable.dicts("points",range(nrstations),0,1,cat='Integer')
     T = LpVariable.dicts("tracks",((i,j) for i in range(nrstations) for j in range(nrstations)), 0, 1,cat='Integer')
 
     ###objective function
-    model += lpSum(A*length[i,j]*T[i,j] + B*costs[i,j]*T[i,j] for i in range(nrstations) for j in range(nrstations))
+    model += lpSum(A*length[i][j]*T[i,j] + B*costs[i][j]*T[i,j] for i in range(nrstations) for j in range(nrstations))
 
     for i in range(nrstations):
         for j in range(nrstations):
             if i==j:
-                model += T[i,j] == 0
+                model += T[i,j] == 0                        #can't go from station to itself
             else:
-                model += P[i] >= T[i,j]
-                if choicetracks[i,j] == 1:
-                    model += T[i,j] == 1
-        model += lpSum(T[i,j] for j in range(nrstations)) >= P[i]
-        if choicepoints[i] == 1:
-            model += P[i] == 1
+                model += P[i] >= T[i,j]                     #a point is passed if a track incident to it is used
+                model += T[i,j] >= choicetracks[i][j]        #use a track if it's in the user's preferences
+                model += T[i,j] <= tracks[i][j]              #if a track doesn't exist don't use it
+
+        model += lpSum(T[i,j] for j in range(nrstations)) >= P[i]       #if a point is passed, at least one track incident to it has to be used
+        model += P[i] >= choicepoints[i]                                #pass a point if it's in the user's preferences
 
     model.solve()
 
+    print(pulp.value(model.objective))
+    print(LpStatus[model.status])
+    for i in range(nrstations):
+        print("P_{} = ".format(i), P[i].varValue)
+        for j in range(nrstations):
+            print("T_{}{} = ".format(i,j), T[i,j].varValue)
 
 
+stations = ['a','b','c']
+
+tracks = [[0,1,1],[1,0,1],[1,1,0]]
+
+choicepoints = [0,0,0]
+
+choicetracks = [[0,1,0],[1,0,0],[0,0,0]]
+
+length = [[0,8,2],[8,0,1],[2,1,0]]
+
+costs = [[0,1,1],[1,0,1],[1,1,0]]
+A, B = 1, 0
+
+sol = lpsolver(stations,tracks,choicepoints,choicetracks,length,costs,A,B)

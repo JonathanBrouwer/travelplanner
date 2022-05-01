7321c6b635801fea1d632993c72c8c6f93eb051b
wcqaguxa <a.k.wolska@student.tudelft.nl>
2022-05-01 03:46:17 +0200

    resolve merge conflicts

--- src/testAPI.py
+++ src/testAPI.py
@@ -9,7 +9,7 @@ class TestClass(unittest.TestCase):
     def test_get_closest_locations(self):
         stations = {Point(0.0, 0.0): "Zero", Point(1.0, 1.0): "One", Point(100.0, 100.0): "Hundred"}
         api = API(stations)
-        result = api.get_closest_station({"lat": 0.6, "lgn": 0.6})
+        result = api.get_closest_station({"lat": 0.6, "lng": 0.6})
         assert result == Station(Point(1.0, 1.0), "One")
 
     def test_get_segment_endpoints(self):
